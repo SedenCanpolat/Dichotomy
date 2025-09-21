2d824be9b40bb58e6e62f6ee549e32316baadbe7
SedenCanpolat <sedencanpolat@gmail.com>
2025-09-21 13:00:25 +0300

    level 2 done?

--- Assets/SedCode/TriggerArea.cs
+++ Assets/SedCode/TriggerArea.cs
@@ -4,14 +4,20 @@ using UnityEngine.AI;
 
 public class TriggerArea : MonoBehaviour
 {
-    public static Action OnTriggerArea;
+    //public static Action OnTriggerArea;
+    private DroneBehaviour _drone;
     void OnTriggerEnter2D(Collider2D collision)
     { 
         NavMeshAgent agent = collision.GetComponent<NavMeshAgent>();
 
         if (agent != null)
         {
-            OnTriggerArea?.Invoke();
+            //OnTriggerArea?.Invoke();
+            _drone = agent.GetComponent<DroneBehaviour>();
+            if (_drone != null)
+            {
+                _drone.Interacted();
+            }
         }
         
     }
