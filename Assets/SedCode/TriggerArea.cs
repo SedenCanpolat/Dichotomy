808abf13a7b3a0089a97aac73613a7cde7bbcd09
SedenCanpolat <sedencanpolat@gmail.com>
2025-09-21 13:35:25 +0300

    dog added

--- Assets/SedCode/TriggerArea.cs
+++ Assets/SedCode/TriggerArea.cs
@@ -6,6 +6,7 @@ public class TriggerArea : MonoBehaviour
 {
     //public static Action OnTriggerArea;
     private DroneBehaviour _drone;
+    private DogBehaviour _dog;
     void OnTriggerEnter2D(Collider2D collision)
     { 
         NavMeshAgent agent = collision.GetComponent<NavMeshAgent>();
@@ -14,9 +15,14 @@ public class TriggerArea : MonoBehaviour
         {
             //OnTriggerArea?.Invoke();
             _drone = agent.GetComponent<DroneBehaviour>();
+            _dog = agent.GetComponent<DogBehaviour>();
             if (_drone != null)
             {
                 _drone.Interacted();
+            }
+            if (_dog != null)
+            {
+                _dog.Interacted();
             }
         }
         
